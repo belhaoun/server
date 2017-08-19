38f8065a672d81316ecd976931632ae4558f7b00
belhaoun <badr.elhaouni@gmail.com>
2017-08-19 16:08:27 -0700

    changer le port

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ app.get('/video/video.mp4', function (req, res) {
 
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(7000, function () {
+  console.log('Example app listening on port 7000!');
 });
